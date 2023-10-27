28e5f22fc467af9866fb2c871711d1225e675a5b
Unknown <gm21007@ues.edu.sv>
2023-10-27 16:56:38 -0600

    modificaciones3
    mas modificaciones

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 #poetry install
-python -m pip install --upgrade pip
+python -m pip install pip --upgrade
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
