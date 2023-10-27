f379e0ac303a4d2842e531fc14239c8986fbd23a
Unknown <gm21007@ues.edu.sv>
2023-10-27 16:39:32 -0600

    arreglo 1
    agregar comandos

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 #poetry install
-#pip install -r requirements.txt
+python -m pip install pip --upgrade
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
