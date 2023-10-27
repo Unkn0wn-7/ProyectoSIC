ece7621e460086c1e28ef4ebe3138ee8c95c4821
Unknown <gm21007@ues.edu.sv>
2023-10-27 16:41:30 -0600

    modificacion3
    tercera modificacion de prueba

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 #poetry install
-python -m pip install pip --upgrade
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
